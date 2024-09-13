111a90ea8548b9e43479aff206dbe9e5712b6fc6
hafijurDia <hafijur@gmail.com>
2024-09-13 10:52:38 +0600

    jdfh

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+
 
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+
 
 const corsOptions = {
   origin: "http://localhost:5173"
